1b6ecf0dfd05e1e24cc0666d7faffa2402f2db9e
znajafi <najafi.z15@gmail.com>
2024-12-24 22:53:46 +0330

    The third session of talwindcss class
    Color object syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    colors: {
+      red: {
+        100: '#cffafe',
+        200: '#a5f3fc',
+        300: '#67e8f9',
+        400: '#22d3ee',
+        500: '#06b6d4',
+        600: '#0891b2',
+        700: '#0e7490',
+        800: '#155e75',
+        900: '#164e63',
+      }, 
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
     extend: {},
   },
   plugins: [],
